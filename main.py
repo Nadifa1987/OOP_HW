e212aa4ac40309ffe150f094fb18c6415f3de69d
Nadifa1987 <nadifa1987@mail.ru>
2022-07-20 23:59:01 +0300

    Задача 4

--- main.py
+++ main.py
@@ -93,3 +93,75 @@ class Reviewer(Mentor):
     def __str__(self):
         res = f'Имя: {self.name}\nФамилия: {self.surname}'
         return res
+
+def ave_rate_all_students(course, *students):
+    student_list = []
+    for student in students:
+        if student.grades.get(course):
+            student_list.extend(student.grades[course])
+    return round(sum(student_list) / len(student_list), 2)
+
+
+def average_rate_all_lecturers(course, *lecturers):
+    lector_list = []
+    for lecturer in lecturers:
+        if lecturer.rate_from_student.get(course):
+            lector_list.extend(lecturer.rate_from_student[course])
+    return round(sum(lector_list) / len(lector_list), 2)
+
+# Студенты
+student1 = Student('Patric', 'Boy', 'm')
+student2 = Student('Venera', 'Sun', 'w')
+
+# Студенты по курсам
+student1.courses_in_progress += ['Python']
+student1.courses_in_progress += ['Jira']
+student2.courses_in_progress += ['Git']
+student2.courses_in_progress += ['Python']
+
+
+# Проверяющие экперты
+reviewer_1 = Reviewer('Dima', 'Mendeleev')
+reviewer_2 = Reviewer('Nic', 'Tesla')
+
+# Эксперты по курсам
+reviewer_1.courses_attached += ['Python']
+reviewer_2.courses_attached += ['Jira']
+reviewer_1.courses_attached += ['Git']
+
+# Оценки за домашку
+reviewer_1.rate_hw(student1, 'Python', 10)
+reviewer_2.rate_hw(student1, 'Jira', 8)
+reviewer_1.rate_hw(student2, 'Git', 10)
+reviewer_1.rate_hw(student2, 'Python', 7)
+reviewer_1.rate_hw(student2, 'Git', 9)
+
+# Лекторы
+lec_1 = Lecturer('Alexandr', 'Block')
+lec_2 = Lecturer('Fedor', 'Dostoevskiy')
+
+# Лекторы по курсам
+lec_1.courses_attached += ['Python']
+lec_1.courses_attached += ['Jira']
+lec_2.courses_attached += ['Python']
+lec_2.courses_attached += ['Jira']
+
+# Оценки лекторам
+student1.rate_lec(lec_1, 'Python', 10)
+student2.rate_lec(lec_2, 'Python', 9)
+student1.rate_lec(lec_2, 'Jira', 8)
+student1.rate_lec(lec_2, 'Python', 6)
+student2.rate_lec(lec_1, 'Python', 9)
+
+print(f'\nСтудент: {student1}')
+print(f'\nОценки: {student1.grades}')
+print(f'\nЛектор: {lec_1}')
+print(f'\nОценки студентов {lec_1.rate_from_student}')
+print(f'\nПроверяющий: {reviewer_1}')
+print()
+print(student2 < lec_1)
+print(student2 > lec_1)
+print()
+course = 'Python'
+print(ave_rate_all_students(course, student1, student2))
+print(average_rate_all_lecturers(course, lec_1, lec_2))
